8c246ec2c5261103f4630577f119fe332a33683f
586837r <586837r@gmail.com>
2018-12-08 15:03:06 +0100

    fixed events triggering more than once

--- nodes/alexa-remote-event.js
+++ nodes/alexa-remote-event.js
@@ -8,27 +8,30 @@ module.exports = function (RED) {
 		tools.assign(node, ['event'], input);
 		tools.assignNode(RED, node, ['account'], input);
 
-		if(node.account){
-			if(node.account.useWsMqtt){
-				node.account.emitter.addListener('alexa-init', () => {
-					node.status({ fill: "yellow", shape: "dot", text: "starting listening" });
-					setTimeout(() => node.status({ fill: "grey", shape: "dot", text: "listening" }), 2000);
-					node.account.alexa.addListener(node.event, val => {
-						node.status({ fill: "blue", shape: "dot", text: "event fired!" });
-						setTimeout(() => node.status({ fill: "grey", shape: "dot", text: "listening" }), 2000);
-						node.send({ payload: val });
-					});
-				});
-				if (node.account.initType !== 'manual') {
-					node.account.initAlexa();
-				}
-			}
-			else {
-				node.status({ fill: "red", shape: "dot", text: "events not supported by account" });
-			}
-		}
+		node.initCallback = function() {
+			node.warn('account init');
+			node.status({ fill: "yellow", shape: "dot", text: "starting listening" });
+			setTimeout(() => node.status({ fill: "grey", shape: "dot", text: "listening" }), 2000);
+			node.account.alexa.addListener(node.event, val => {
+				node.status({ fill: "blue", shape: "dot", text: "event fired!" });
+				setTimeout(() => node.status({ fill: "grey", shape: "dot", text: "listening" }), 2000);
+				node.send({ payload: val });
+			});
+		};
 
 		node.on('close', function (removed, done) { this.status({}); done(); });
+
+		if(!node.account)
+			return node.status({ fill: "red", shape: "dot", text: "account missing" });
+
+		if(!node.account.useWsMqtt)
+			return node.status({ fill: "red", shape: "dot", text: "events not supported by account" });
+
+		node.account.emitter.removeListener('alexa-init', node.initCallback);
+		node.account.emitter.addListener('alexa-init', node.initCallback);
+		if (node.account.initType !== 'manual') {
+			node.account.initAlexa();
+		}
 	}
 	RED.nodes.registerType("alexa-remote-event", AlexaRemoteEventNode)
 }
